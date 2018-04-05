411ee2683da3a8bfe99a5b400e3c47fd0bc1dc11
Jackson Kontny <jackson.kontny@apervita.com>
2018-04-04 20:40:54 -0500

    sync home direcotry and fix timing issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,8 @@
 apt-get update
 apt-get install make
+sudo apt install ntpdate
+sudo ntpdate ntp.ubuntu.com
+sudo timedatectl set-ntp on
+sudo service ntp stop
+sudo ntpd -gq
+sudo service ntp start
